a6215cdbe1abafc2898e3d3dcf3dd4df0c91ad2b
Zerohertz <ohg3417@gmail.com>
2023-11-04 01:31:06 +0900

    :hammer: Update: Version

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Zerohertz"
 author = "Zerohertz"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.1.1"
+release = "v0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
