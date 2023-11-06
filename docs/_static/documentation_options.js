7c2504092bb5cb1604101b245cb5557d8bb92212
Zerohertz <ohg3417@gmail.com>
2023-11-06 01:58:13 +0000

    :memo: Docs: Build Sphinx (#16)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.3',
+    VERSION: 'PR-16',
     LANGUAGE: 'ko',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
