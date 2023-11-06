b7ed3ecf8edba5a4f88b8030de16f16f077f451a
Zerohertz <ohg3417@gmail.com>
2023-11-06 14:49:28 +0900

    :hammer: Fix: Discord Webhook URL

--- test/test_api.py
+++ test/test_api.py
@@ -3,13 +3,13 @@ import zerohertzLib as zz
 
 def test_discord():
     for response in zz.api.send_discord_message(
-        "https://discord.com/api/webhooks/1147058609130311690/geDqeubwN5vKomEExJnFckS9SSYFJgvdJh2IX-02kHWmWjyGgzCebEvLzyj6Gn1f92fd",
+        "https://discord.com/api/webhooks/1170962638583373904/xVJKW1KkNo7Pc1HykJ85cHs_4SvRkKCbOvbf1qe1j8QXOnJyTGyJy8T7sI7kvfA8SGb-",
         "Testing..." * 1,
     ):
         assert response.status_code == 204
 
     for response in zz.api.send_discord_message(
-        "https://discord.com/api/webhooks/1147058609130311690/geDqeubwN5vKomEExJnFckS9SSYFJgvdJh2IX-02kHWmWjyGgzCebEvLzyj6Gn1f92fd",
+        "https://discord.com/api/webhooks/1170962638583373904/xVJKW1KkNo7Pc1HykJ85cHs_4SvRkKCbOvbf1qe1j8QXOnJyTGyJy8T7sI7kvfA8SGb-",
         "Testing..." * 200,
     ):
         assert response.status_code == 204
