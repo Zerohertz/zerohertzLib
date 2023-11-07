fc4ae39d7a0c76e23a3ebf56ad3bda0f7b4c2044
Zerohertz <ohg3417@gmail.com>
2023-11-07 20:59:14 +0900

    :tada: Feat: Client of Triton Inference Server

---
+++ zerohertzLib/MLOps/__init__.py
@@ -0,0 +1,3 @@
+from zerohertzLib.MLOps.triton import tritonClientK8s, tritonClientURL
+
+__all__ = ["tritonClientK8s", "tritonClientURL"]
--- zerohertzLib/__init__.py
+++ zerohertzLib/__init__.py
@@ -1,3 +1,3 @@
-from zerohertzLib import algorithm, api, plot, vision
+from zerohertzLib import MLOps, algorithm, api, plot, vision
 
 __version__ = "v0.1.6"
