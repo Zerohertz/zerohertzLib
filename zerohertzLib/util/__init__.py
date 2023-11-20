120a8a74e28538c111bbcbc6597b386d2f83e974
Zerohertz <ohg3417@gmail.com>
2023-11-20 23:08:08 +0900

    :sparkles: Feat: Read CSV

--- zerohertzLib/util/__init__.py
+++ zerohertzLib/util/__init__.py
@@ -1,5 +1,5 @@
-from zerohertzLib.util.csv import write_csv
+from zerohertzLib.util.csv import read_csv, write_csv
 from zerohertzLib.util.data import MakeData
 from zerohertzLib.util.json import Json, JsonDir, write_json
 
-__all__ = ["Json", "JsonDir", "MakeData", "write_csv", "write_json"]
+__all__ = ["Json", "JsonDir", "MakeData", "write_csv", "write_json", "read_csv"]
--- zerohertzLib/util/csv.py
+++ zerohertzLib/util/csv.py
@@ -1,6 +1,55 @@
-import json
 import os
+from collections import defaultdict
 from typing import Any, Dict, List, Optional, Union
+
+
+def read_csv(
+    path: str, header: Optional[bool] = True
+) -> Dict[Union[int, str], List[str]]:
+    """CSV (Comma-Separated Values) 혹은 TSV (Tab-Separated Values)를 작성하는 함수
+
+    Args:
+        path (``str``): 입력될 CSV 혹은 TSV 경로 및 파일명
+        header (``Optional[bool]``): Header의 존재 유무
+
+    Returns:
+        ``Dict[str, List[str]]``: Header의 값을 기반으로 column에 따라 `List` 로 구성
+
+    Note:
+        Header가 존재하지 않는 경우 `0` 부터 차례대로 key 값 정의
+
+    Examples:
+        >>> zz.util.read_csv("star_craft.csv")
+        defaultdict(<class 'list'>, {'id': ['5hi9', 'gor2', 'gk03'], '종족': ['프로토스', '테란', '저그'], '점수': ['1248', '2309', '291']})
+        >>> zz.util.read_csv("star_craft.tsv")
+        defaultdict(<class 'list'>, {'id': ['5hi9', 'gor2', 'gk03'], '종족': ['프로토스', '테란', '저그'], '점수': ['1248', '2309', '291']})
+        >>> zz.util.read_csv("star_craft.csv", header=False)
+        defaultdict(<class 'list'>, {0: ['id', '5hi9', 'gor2', 'gk03'], 1: ['종족', '프로토스', '테란', '저그'], 2: ['점수', '1248', '2309', '291']})
+        >>> zz.util.read_csv("star_craft.tsv", header=False)
+        defaultdict(<class 'list'>, {0: ['id', '5hi9', 'gor2', 'gk03'], 1: ['종족', '프로토스', '테란', '저그'], 2: ['점수', '1248', '2309', '291']})
+    """
+    data = defaultdict(list)
+    keys = []
+    with open(path, "r", encoding="utf-8") as f:
+        raw = f.readlines()
+    if path.endswith(".csv"):
+        delimiter = ","
+    elif path.endswith(".tsv"):
+        delimiter = "\t"
+    else:
+        raise Exception("File is not CSV or TSV")
+    if header:
+        r = raw[0]
+        for key in r.strip().split(delimiter):
+            keys.append(key)
+        raw = raw[1:]
+    else:
+        for key in range(len(raw[0].strip().split(delimiter))):
+            keys.append(key)
+    for r in raw:
+        for k, d in zip(keys, r.strip().split(delimiter)):
+            data[k].append(d)
+    return data
 
 
 def write_csv(data: List[List[Any]], path: str, tsv: Optional[bool] = False) -> str:
