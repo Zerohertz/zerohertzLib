e4c6099dbd37aec48998519e9fb48279ab448a0c
Zerohertz <ohg3417@gmail.com>
2024-11-04 17:53:25 +0900

    :bug: fix: test of api (HTTP response 429)

--- test/test_api.py
+++ test/test_api.py
@@ -1,4 +1,6 @@
 import os
+import random
+import time
 
 import zerohertzLib as zz
 
@@ -14,18 +16,21 @@ data = os.path.join(tmp, "data")
 
 def test_discord_message():
     discord = zz.api.Discord(DISCORD_WEBHOOK_URL)
+    time.sleep(random.randrange(10))
     for response in discord.message("Testing..."):
         assert response.status_code == 204
 
 
 def test_discord_messages():
     discord = zz.api.Discord(DISCORD_WEBHOOK_URL)
+    time.sleep(random.randrange(10))
     for response in discord.message("Testing..." * 200):
         assert response.status_code == 204
 
 
 def test_discord_image():
     discord = zz.api.Discord(DISCORD_WEBHOOK_URL)
+    time.sleep(random.randrange(10))
     response = discord.image(f"{data}/test.jpg")
     assert response.status_code == 200
 
@@ -34,6 +39,7 @@ def test_slack_webhook():
     slack = zz.api.SlackWebhook(
         SLACK_WEBHOOK_URL, "test", name="Test Webhook", icon_emoji="wrench"
     )
+    time.sleep(random.randrange(10))
     response = slack.message("Testing...")
     assert response.status_code == 200
 
@@ -42,6 +48,7 @@ def test_slack_bot_message():
     slack = zz.api.SlackBot(
         SLACK_BOT_TOKEN, "test", name="Test Bot", icon_emoji="hammer"
     )
+    time.sleep(random.randrange(10))
     response = slack.message("Testing...")
     assert response.status_code == 200
 
@@ -50,6 +57,7 @@ def test_slack_bot_file():
     slack = zz.api.SlackBot(
         SLACK_BOT_TOKEN, "test", name="Test Bot", icon_emoji="hammer"
     )
+    time.sleep(random.randrange(10))
     response = slack.file(f"{data}/test.jpg")
     assert response.status_code == 200
 
