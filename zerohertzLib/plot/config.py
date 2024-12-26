8b4784ffaf7fc8872eb94e13bf2f84533da9b0c9
Zerohertz <ohg3417@gmail.com>
2024-12-26 21:34:26 +0900

    :bento: style: pylint (W0105)

--- zerohertzLib/plot/config.py
+++ zerohertzLib/plot/config.py
@@ -21,19 +21,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-"""
-zerohertzLib.plot.figure(): False
-zerohertzLib.plot.savefig(): True
-"""
+# zerohertzLib.plot.figure(): False
+# zerohertzLib.plot.savefig(): True
 SAVE = True
 
-"""
-zerohertzLib.plot.plot()
-zerohertzLib.plot.scatter()
-"""
+# zerohertzLib.plot.plot()
 # from matplotlib import pyplot as plt
 # list(plt.Line2D.lineStyles.keys())
 LINESTYLE = ["-", "--", "-.", ":"]
+
+# zerohertzLib.plot.plot()
+# zerohertzLib.plot.scatter()
 # import matplotlib.markers as mmarkers
 # markers = list(mmarkers.MarkerStyle.markers.keys())
 MARKER = ["o", "v", "^", "s", "p", "*", "x"]
