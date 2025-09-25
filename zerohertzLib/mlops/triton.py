7ccb615d7ce6ae2c69d151d003ab54fc653e151d
Zerohertz <ohg3417@gmail.com>
2025-09-26 01:27:36 +0900

    :sparkles: feat: dynamic batching in triton inference server
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- zerohertzLib/mlops/triton.py
+++ zerohertzLib/mlops/triton.py
@@ -2,10 +2,12 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025 Zerohertz (Hyogeun Oh)
 
 import json
-import traceback
+import time
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from typing import Any
 
+import numpy as np
 import tritonclient.grpc as grpcclient
 from loguru import logger
 from numpy.typing import DTypeLike, NDArray
@@ -72,7 +74,13 @@ class TritonClientURL(grpcclient.InferenceServerClient):
         assert len(inputs) == len(args)
         triton_inputs = []
         for input_info, arg in zip(inputs, args):
-            triton_inputs.append(self._set_input(input_info, arg))
+            triton_inputs.append(
+                self._set_input(
+                    input_info,
+                    arg,
+                    self.configs[model]["config"].get("max_batch_size", None),
+                )
+            )
         triton_outputs = []
         for output in outputs:
             triton_outputs.append(grpcclient.InferRequestedOutput(output["name"]))
@@ -90,15 +98,21 @@ class TritonClientURL(grpcclient.InferenceServerClient):
             self.configs[model] = self.get_model_config(model, as_json=True)
 
     def _set_input(
-        self, input_info: dict[str, list[int]], value: NDArray[DTypeLike]
+        self,
+        input_info: dict[str, list[int]],
+        value: NDArray[DTypeLike],
+        max_batch_size: int | None,
     ) -> grpcclient._infer_input.InferInput:
-        if "dims" in input_info.keys() and len(input_info["dims"]) != len(value.shape):
-            logger.warning(
-                "Expected dimension length of input (%d) does not match the input dimension length (%d) [input dimension: %s]",
-                len(input_info["dims"]),
-                len(value.shape),
-                value.shape,
-            )
+        if "dims" in input_info.keys():
+            if max_batch_size is None:
+                if len(input_info["dims"]) != len(value.shape):
+                    logger.warning(
+                        f"""Expected dimension length of input ({len(input_info["dims"])}) does not match the input dimension length ({len(value.shape)}) [input dimension: {value.shape}]""",
+                    )
+            elif len(input_info["dims"]) + 1 != len(value.shape):
+                logger.warning(
+                    f"""Expected dimension length of input ({len(input_info["dims"]) + 1}) does not match the input dimension length ({len(value.shape)}) [input dimension: {value.shape}]""",
+                )
         value = value.astype(triton_to_np_dtype(input_info["data_type"][5:]))
         return grpcclient.InferInput(
             input_info["name"],
@@ -161,7 +175,7 @@ class TritonClientURL(grpcclient.InferenceServerClient):
         if sortby:
             table.sortby = sortby
         table.reversesort = reverse
-        logger.info("\n%s", str(table))
+        logger.info(f"\n{table}")
 
     def load_model(
         self,
@@ -255,68 +269,84 @@ class BaseTritonPythonModel(ABC):
     Note:
         Abstract Base Class: Model의 추론을 수행하는 abstract method `_inference` 정의 후 사용
 
-    Tip:
-        Logger의 색상 적용을 위해 아래와 같은 환경 변수 정의 필요
-
-        ```yaml
-        spec:
-          template:
-            spec:
-              containers:
-                - name: ${NAME}
-                  ...
-                  env:
-                    - name: "FORCE_COLOR"
-                      value: "1"
-                  ...
-        ```
-
     Examples:
         `model.py`:
             ```python
             class TritonPythonModel(zz.mlops.BaseTritonPythonModel):
-                def initialize:
-                    super().initialize(args, 10)
+                def initialize(self, args: dict[str, str]) -> None:
+                    super().initialize(args)
                     self.model = Model(cfg)
 
-                def _inference:
-                    return self.model(input_image)
+                def _inference(input) -> tuple[Any]:
+                    return self.model(input)
             ```
 
-        Normal Logs:
+        Normal Logs (Without Batching):
             ```
-            [04/04/24 00:00:00] INFO     [MODEL] Initialize                        triton.py:*
-            [04/04/24 00:00:00] INFO     [MODEL] Called                            triton.py:*
-                                DEBUG    [MODEL] inputs: (3, 3, 3)                 triton.py:*
-                                INFO     [MODEL] Inference start                   triton.py:*
-                                DEBUG    [MODEL] outputs: (10,) (20,)              triton.py:*
-                                INFO     [MODEL] Inference completed               triton.py:*
+            2025-09-25 16:06:51.904 | INFO     | zerohertzLib.mlops.triton:initialize:* - Initialize: {
+                "name": "...",
+                "platform": "",
+                "backend": "python",
+                "runtime": "",
+                "version_policy": {
+                    "latest": {
+                        "num_versions": 1
+                    }
+                },
+                "max_batch_size": 0,
+            ...
+            2025-09-25 16:22:48.226 | INFO     | zerohertzLib.mlops.triton:execute:* - Called
+            2025-09-25 16:22:48.234 | DEBUG    | zerohertzLib.mlops.triton:_get_inputs:* - inputs: images=(2078, 1470, 3)
+            2025-09-25 16:22:48.234 | INFO     | zerohertzLib.mlops.triton:execute:* - Inference start
+            2025-09-25 16:22:49.026 | INFO     | zerohertzLib.mlops.triton:execute:* - Inference completed (0.79s)
+            2025-09-25 16:22:49.026 | DEBUG    | zerohertzLib.mlops.triton:_set_outputs:* - outputs: boxes=(12, 4), scores=(12,), labels=(12,)
+            ```
+
+        Normal Logs (With Batching):
+            ```
+            2025-09-25 16:24:34.224 | INFO     | zerohertzLib.mlops.triton:execute:* - Called
+            2025-09-25 16:24:34.232 | DEBUG    | zerohertzLib.mlops.triton:_get_inputs:* - inputs: images=(9, 1000, 1000, 3)
+            2025-09-25 16:24:34.232 | INFO     | zerohertzLib.mlops.triton:execute:* - Inference start
+            2025-09-25 16:24:34.486 | INFO     | zerohertzLib.mlops.triton:execute:* - Inference completed (0.25s)
+            2025-09-25 16:24:34.486 | DEBUG    | zerohertzLib.mlops.triton:_set_outputs:* - outputs (0 ~ 3): boxes=(6, 4), (6, 4), (6, 4), scores=(6,), (6,), (6,), labels=(6,), (6,), (6,), batch_index=(6,), (6,), (6,)
+            2025-09-25 16:24:34.486 | DEBUG    | zerohertzLib.mlops.triton:_set_outputs:* - outputs (3 ~ 6): boxes=(6, 4), (6, 4), (6, 4), scores=(6,), (6,), (6,), labels=(6,), (6,), (6,), batch_index=(6,), (6,), (6,)
+            2025-09-25 16:24:34.486 | DEBUG    | zerohertzLib.mlops.triton:_set_outputs:* - outputs (6 ~ 9): boxes=(6, 4), (6, 4), (6, 4), scores=(6,), (6,), (6,), labels=(6,), (6,), (6,), batch_index=(6,), (6,), (6,)
             ```
 
         Error Logs:
             ```
-            [04/04/24 00:00:00] INFO     [MODEL] Called                            triton.py:*
-                                INFO     [MODEL] Inference start                   triton.py:*
-                                CRITICAL [MODEL] Hello, World!                     triton.py:*
-                                        ====================================================================================================
-                                        Traceback:
-                                        File "/usr/local/lib/python3.8/dist-packages/zerohertzLib/mlops/triton.py", line *, in execute
-                                            outputs = self._inference(*inputs)
-                                        File "/models/model/*/model.py", line *, in _inference
-                                            raise Exception("Hello, World!")
-                                        Exception: Hello, World!
-                                        ====================================================================================================
+            2025-09-25 16:26:32.004 | ERROR    | zerohertzLib.mlops.triton:execute:* - zerohertzLib!
+            Traceback (most recent call last):
+            > File "/usr/local/lib/python3.10/dist-packages/zerohertzLib/mlops/triton.py", line 371, in execute
+                outputs = self._inference(**inputs)
+                        |    |            -> {'images': array([[[ 38,  38,  38],
+                        |    |                       [ 37,  37,  37],
+                        |    |                       [ 37,  37,  37],
+                        |    |                       ...,
+                        |    |                       [255, 255, 255],
+                        |    |                ...
+                        |    -> <function TritonPythonModel._inference at 0x7f106f48f400>
+                        -> <1.model.TritonPythonModel object at 0x7f121fa1f010>
+
+            File "/models/docling_layout_old_static/1/model.py", line 34, in _inference
+                raise Exception("zerohertzLib!")
+            Exception: zerohertzLib!
             ```
     """
 
-    def initialize(self, args: dict[str, Any]) -> None:
+    def initialize(self, args: dict[str, str]) -> None:
         """Triton Inference Server 시작 시 수행되는 method
 
         Args:
             args: `config.pbtxt` 에 포함된 model의 정보
         """
         self.cfg = json.loads(args["model_config"])
-        logger.info("Initialize")
+        logger.info(f"Initialize: {json.dumps(self.cfg, indent=4)}")
+        self.device = "cpu"
+        device = args.get("model_instance_device_id", None)
+        if device is not None:
+            self.device = f"cuda:{device}"
+        self.max_batch_size = self.cfg.get("max_batch_size", 0)
 
     def execute(self, requests: list[Any]) -> list[Any]:
         """Triton Inference Server 호출 시 수행되는 method
@@ -327,64 +357,107 @@ class BaseTritonPythonModel(ABC):
         Returns:
             Client에 응답할 model의 추론 결과
         """
-        responses = []
-        for request in requests:
-            try:
-                logger.info("Called")
-                inputs = self._get_inputs(request)
-                logger.debug(
-                    "inputs: %s", " ".join([str(input_.shape) for input_ in inputs])
+        logger.info("Called")
+        try:
+            inputs, batch_index = self._get_inputs(requests=requests)
+            logger.info("Inference start")
+            start = time.time()
+            outputs = self._inference(**inputs)
+            end = time.time()
+            logger.info(f"Inference completed ({end - start:.2f}s)")
+            if not isinstance(outputs, tuple):
+                outputs = tuple([outputs])
+            responses = self._set_outputs(outputs=outputs, batch_index=batch_index)
+        except Exception as exc:
+            logger.exception(exc)
+            responses = [
+                pb_utils.InferenceResponse(
+                    output_tensors=[], error=pb_utils.TritonError(exc)
                 )
-                logger.info("Inference start")
-                outputs = self._inference(*inputs)
-                if not isinstance(outputs, tuple):
-                    outputs = tuple([outputs])
-                logger.debug(
-                    "outputs: %s", " ".join([str(output.shape) for output in outputs])
-                )
-                logger.info("Inference completed")
-                response = self._set_outputs(outputs)
-                responses.append(response)
-            except Exception as error:
-                message = (
-                    str(error)
-                    + "\n"
-                    + "=" * 100
-                    + "\n"
-                    + str(traceback.format_exc())
-                    + "=" * 100
-                )
-                logger.critical(message)
-                responses.append(
-                    pb_utils.InferenceResponse(
-                        output_tensors=[], error=pb_utils.TritonError(message)
-                    )
-                )
+                for _ in requests
+            ]
         return responses
 
-    def _get_inputs(self, request: Any) -> list[NDArray[DTypeLike]]:
-        inputs = []
-        for input_ in self.cfg["input"]:
-            inputs.append(
-                pb_utils.get_input_tensor_by_name(request, input_["name"]).as_numpy()
-            )
-        return inputs
+    def _get_inputs(
+        self, requests: list[Any]
+    ) -> tuple[dict[str, NDArray[DTypeLike]], list[int]]:
+        batch_index = [0]
+        _inputs = defaultdict(list)
+        for request in requests:
+            for _input in self.cfg["input"]:
+                __input = pb_utils.get_input_tensor_by_name(
+                    request, _input["name"]
+                ).as_numpy()
+                if 0 < self.max_batch_size:
+                    batch_index.append(batch_index[-1] + __input.shape[0])
+                _inputs[_input["name"]].append(__input)
+        inputs = {}
+        for key, value in _inputs.items():
+            inputs[key] = np.concatenate(value, axis=0)
+        logger.debug(
+            "inputs: "
+            + ", ".join([f"{key}={value.shape}" for key, value in inputs.items()])
+        )
+        return inputs, batch_index
 
-    def _set_outputs(self, outputs: tuple[NDArray[DTypeLike]]) -> Any:
+    def _set_outputs(self, outputs: tuple[Any], batch_index: list[int]) -> list[Any]:
+        responses = []
+        if 0 < self.max_batch_size:
+            for index in range(len(batch_index) - 1):
+                batch_tensors = defaultdict(list)
+                for batch in range(batch_index[index], batch_index[index + 1]):
+                    for _output, value in zip(self.cfg["output"], outputs):
+                        _value = value[batch]
+                        if _output["name"] == "batch_index":
+                            _value -= batch_index[index]
+                        batch_tensors[_output["name"]].append(_value)
+                output_tensors = []
+                for _output in self.cfg["output"]:
+                    value = np.concatenate(batch_tensors[_output["name"]], axis=0)
+                    output_tensors.append(
+                        pb_utils.Tensor(
+                            _output["name"],
+                            value.astype(
+                                pb_utils.triton_string_to_numpy(_output["data_type"])
+                            ),
+                        )
+                    )
+                responses.append(
+                    pb_utils.InferenceResponse(output_tensors=output_tensors)
+                )
+                logger.debug(
+                    f"outputs ({batch_index[index]} ~ {batch_index[index + 1]}): "
+                    + ", ".join(
+                        [
+                            f"{key}="
+                            + ", ".join([f"{_value.shape}" for _value in value])
+                            for key, value in batch_tensors.items()
+                        ]
+                    )
+                )
+            return responses
         output_tensors = []
-        for output, value in zip(self.cfg["output"], outputs):
+        for _output, value in zip(self.cfg["output"], outputs):
             output_tensors.append(
                 pb_utils.Tensor(
-                    output["name"],
-                    value.astype(pb_utils.triton_string_to_numpy(output["data_type"])),
+                    _output["name"],
+                    value.astype(pb_utils.triton_string_to_numpy(_output["data_type"])),
                 )
             )
-        return pb_utils.InferenceResponse(output_tensors=output_tensors)
+        responses.append(pb_utils.InferenceResponse(output_tensors=output_tensors))
+        logger.debug(
+            "outputs: "
+            + ", ".join(
+                [
+                    f"""{key["name"]}={value.shape}"""
+                    for key, value in zip(self.cfg["output"], outputs)
+                ]
+            )
+        )
+        return responses
 
     @abstractmethod
-    def _inference(
-        self, *inputs: NDArray[DTypeLike]
-    ) -> NDArray[DTypeLike] | tuple[NDArray[DTypeLike]]:
+    def _inference(self, **inputs: NDArray[DTypeLike]) -> Any | tuple[Any]:
         """
         Model 추론을 수행하는 private method (상속을 통한 재정의 필수)
 
@@ -394,7 +467,7 @@ class BaseTritonPythonModel(ABC):
         Returns:
             Model의 추론 결과
         """
-        ...
+        pass
 
     def finalize(self) -> None:
         """Triton Inference Server 종료 시 수행되는 method"""
