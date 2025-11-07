7dfe4e8acee278a681a70979523340417028640b
Zerohertz <ohg3417@gmail.com>
2025-11-07 18:07:28 +0900

    :ship: test: string input for triton inference server
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- test/data/triton-inference-server/dynamic_batching/1/model.py
+++ test/data/triton-inference-server/dynamic_batching/1/model.py
@@ -6,14 +6,19 @@ import zerohertzLib as zz
 
 class TritonPythonModel(zz.mlops.BaseTritonPythonModel):
     def _inference(
-        self, images: NDArray[DTypeLike], boxes: NDArray[DTypeLike]
+        self,
+        images: NDArray[DTypeLike],
+        boxes: NDArray[DTypeLike],
+        texts: NDArray[DTypeLike],
+        checks: NDArray[DTypeLike],
     ) -> tuple[list[NDArray[DTypeLike]]]:
         batch_size = images.shape[0]
-        _boxes, scores, texts, batch_index = [], [], [], []
+        _boxes, _scores, _texts, _checks, batch_index = [], [], [], [], []
         for batch in range(batch_size):
             objects = np.random.randint(1, 10)
             _boxes.append(np.random.randint(0, 1000, size=(objects, 4)))
-            scores.append(np.random.uniform(0.0, 1.0, size=(objects)))
-            texts.append(np.array(["test" * len(objects)], dtype=np.object_))
+            _scores.append(np.random.uniform(0.0, 1.0, size=(objects)))
+            _texts.append(np.array(["test"] * objects, dtype=object))
+            _checks.append(np.random.randint(0, 2, size=(objects), dtype=bool))
             batch_index.append(np.array([batch] * objects))
-        return _boxes, scores, texts, batch_index
+        return _boxes, _scores, _texts, _checks, batch_index
--- test/data/triton-inference-server/without_batching/1/model.py
+++ test/data/triton-inference-server/without_batching/1/model.py
@@ -6,10 +6,15 @@ import zerohertzLib as zz
 
 class TritonPythonModel(zz.mlops.BaseTritonPythonModel):
     def _inference(
-        self, images: NDArray[DTypeLike], boxes: NDArray[DTypeLike]
-    ) -> tuple[NDArray[DTypeLike]]:
+        self,
+        images: NDArray[DTypeLike],
+        boxes: NDArray[DTypeLike],
+        texts: NDArray[DTypeLike],
+        checks: NDArray[DTypeLike],
+    ) -> tuple[list[NDArray[DTypeLike]]]:
         return (
             np.random.randint(0, 1000, size=(boxes.shape[0], 4)),
             np.random.uniform(0.0, 1.0, size=(boxes.shape[0])),
-            np.array(["test" * boxes.shape[0]], dtype=np.object_),
+            np.array(["test"] * boxes.shape[0], dtype=object),
+            np.random.randint(0, 2, size=(boxes.shape[0]), dtype=bool),
         )
